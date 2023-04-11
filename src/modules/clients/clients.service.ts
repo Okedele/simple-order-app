12debb59f7f1f7ed716cb900aeba78b5c421e00c
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-04-11 15:26:21 +0100

    fix: changed create client method

--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -19,19 +19,31 @@ export class ClientsService {
     this.axiosInstance = axios.create({
         baseURL: configService.get("b54_API"),
         headers: {
-            Authorization: `${configService.get<string>("b54_API_KEY")}`,
+            Authorization: process.env.b54_API_KEY,
             "Content-Type": 'application/json'
         }
     })
-    this.customerPartnerId = configService.get<string>("b54_CUSTOMER_PARTNER_ID")
+    this.customerPartnerId = process.env.b54_CUSTOMER_PARTNER_ID
   }
 
   async create(createClientDto: CreateClientDto) {
     try {
-      let result = await this.axiosInstance.get(`customer_partners/${this.customerPartnerId}/customers`)
-      if(result?.data?.status != 'success') throw new HttpException('unable to create customer', HttpStatus.INTERNAL_SERVER_ERROR)
-      let newClient = this.clientRepository.create(createClientDto)
-      return await this.clientRepository.save(newClient);
+      const {id_type: unique_id_type, id_value: unique_id, ...clientData} = createClientDto
+      const client = await this.clientRepository.findOne({where: {
+        id_type: unique_id_type, id_value: unique_id,
+      }})
+      if (client) {
+        return client
+      } else {
+        let result = await this.axiosInstance.post(`customer_partners/${this.customerPartnerId}/customers`, {
+          unique_id_type,
+          unique_id,
+          ...clientData
+        })
+        if(result?.data?.status != 'success') throw new HttpException('unable to create customer', HttpStatus.INTERNAL_SERVER_ERROR)
+        let newClient = this.clientRepository.create(createClientDto)
+        return await this.clientRepository.save(newClient);
+      }
     } catch(error) {
       return error.message
     }
