4eab9b4ff4a9bae1a9d5079a41fb521e61634ec9
yakusoku_suru <okedelep@gmail.com>
2023-04-11 14:32:18 +0100

    added modules for clients and payments

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { OrdersModule } from './modules/orders/orders.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     OrdersModule,
+    ClientsModule,
+    PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
