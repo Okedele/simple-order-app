188646d97f44cfa91df0781758ab666b2248d0b0
yakusoku_suru <okedelep@gmail.com>
2023-04-11 12:40:42 +0100

    created sample CRUD order module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
