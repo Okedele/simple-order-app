7ddceb6cad1112f3dcc7b311f1f26e003f644a3d
arkmadj <ahmadjinadu@gmail.com>
2023-04-11 21:10:17 +0100

    added order_reference and amount

--- src/modules/payments/dto/create-payment.dto.ts
+++ src/modules/payments/dto/create-payment.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreatePaymentDto {
+  @IsString()
+  @IsNotEmpty()
+  order_reference: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+}
